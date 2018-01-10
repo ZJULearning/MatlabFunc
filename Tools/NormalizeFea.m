75d44bd7cc3ec5e939dd6f5fa2ec6ef552719862
Deng Cai <dengcai78@qq.com>
2018-01-10 13:10:47 +0800

    normalizedfea

--- Tools/NormalizeFea.m
+++ Tools/NormalizeFea.m
@@ -1,27 +1,38 @@
-function fea = NormalizeFea(fea,row)
+function fea = NormalizeFea(fea,row,norm)
 % if row == 1, normalize each row of fea to have unit norm;
 % if row == 0, normalize each column of fea to have unit norm;
 %
+% if norm == p, use p-norm, default norm=2;
+% 
+%   version 4.0 --May/2016 
 %   version 3.0 --Jan/2012 
 %   version 2.0 --Jan/2012 
 %   version 1.0 --Oct/2003 
 %
 %   Written by Deng Cai (dengcai AT gmail.com)
-%
+%              Wei Qian (qwqjzju@gmail.com)
 
 if ~exist('row','var')
     row = 1;
 end
 
+if ~exist('norm','var')
+    norm = 2;
+end
+if norm < 1
+    error('It is not a norm when p small than 1!');
+end
+
 if row
     nSmp = size(fea,1);
-    feaNorm = max(1e-14,full(sum(fea.^2,2)));
-    fea = spdiags(feaNorm.^-.5,0,nSmp,nSmp)*fea;
+    feaNorm = max(1e-14,full(sum(abs(fea).^norm,2)));
+    fea = spdiags(feaNorm.^-(1./norm),0,nSmp,nSmp)*fea;
 else
     nSmp = size(fea,2);
-    feaNorm = max(1e-14,full(sum(fea.^2,1))');
-    fea = fea*spdiags(feaNorm.^-.5,0,nSmp,nSmp);
+    feaNorm = max(1e-14,full(sum(abs(fea).^norm,1))');
+    fea = fea*spdiags(feaNorm.^-(1./norm),0,nSmp,nSmp);
 end
+
             
 return;
 
